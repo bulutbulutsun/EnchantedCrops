9e7a46a956f5aae32bac711fa73f51de51f0c6a1
bulutbulutsun <122382801+bulutbulutsun@users.noreply.github.com>
2025-01-06 23:47:03 +0300

    Implemented pumpkin, melon and nether wart support

--- src/bulutbulutsun/Loader.php
+++ src/bulutbulutsun/Loader.php
@@ -4,8 +4,16 @@ namespace bulutbulutsun;
 
 use bulutbulutsun\listener\EventListener;
 use bulutbulutsun\task\GrowTask;
+use pocketmine\block\BlockTypeIds;
+use pocketmine\block\BlockTypeTags;
 use pocketmine\block\Crops;
+use pocketmine\block\MelonStem;
+use pocketmine\block\NetherWartPlant;
+use pocketmine\block\PumpkinStem;
+use pocketmine\block\Stem;
 use pocketmine\block\utils\BlockEventHelper;
+use pocketmine\block\VanillaBlocks;
+use pocketmine\math\Facing;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\ClosureTask;
 use pocketmine\world\World;
@@ -85,21 +93,65 @@ class Loader extends PluginBase{
     {
         $world = $this->getServer()->getWorldManager()->getWorldByName($world);
         if ($world instanceof World) {
-            if (!$world->isLoaded()){
+            if (!$world->isLoaded()) {
                 $this->getServer()->getWorldManager()->loadWorld($world->getFolderName());
             }
-            if (!$world->isChunkLoaded($x,$y)){
-                $world->loadChunk($x,$y);
+            if (!$world->isChunkLoaded($x, $y)) {
+                $world->loadChunk($x, $y);
             }
-            $crop = $world->getBlockAt($x,$y,$z);
-            if ($crop instanceof Crops){
-                if ($crop->getAge() < 7){
+            $crop = $world->getBlockAt($x, $y, $z);
+            if ($crop instanceof Crops) {
+                if ($crop->getAge() < Crops::MAX_AGE) {
                     $tempAge = $crop->getAge() + mt_rand(2, 5);
-                    if($tempAge > Crops::MAX_AGE){
+                    if ($tempAge > Crops::MAX_AGE) {
                         $tempAge = Crops::MAX_AGE;
                     }
                     $newcrop = $crop->setAge($tempAge);
-                    BlockEventHelper::grow($crop,$newcrop, null);
+                    BlockEventHelper::grow($crop, $newcrop, null);
+                }
+            }
+            if ($crop instanceof NetherWartPlant){
+                if ($crop->getAge() < NetherWartPlant::MAX_AGE) {
+                    $tempAge = $crop->getAge() + mt_rand(2, 5);
+                    if ($tempAge > NetherWartPlant::MAX_AGE) {
+                        $tempAge = NetherWartPlant::MAX_AGE;
+                    }
+                    $newcrop = $crop->setAge($tempAge);
+                    BlockEventHelper::grow($crop, $newcrop, null);
+                }
+            }
+            if ($crop instanceof PumpkinStem) {
+                if ($crop->getAge() >= PumpkinStem::MAX_AGE) {
+                    $grow = VanillaBlocks::PUMPKIN();
+                    foreach(Facing::HORIZONTAL as $side){
+                        if($crop->getSide($side)->hasSameTypeId($grow)){
+                            return;
+                        }
+                    }
+                    $facing = Facing::HORIZONTAL[array_rand(Facing::HORIZONTAL)];
+                    $side = $crop->getSide($facing);
+                    if($side->getTypeId() === BlockTypeIds::AIR && $side->getSide(Facing::DOWN)->hasTypeTag(BlockTypeTags::DIRT)){
+                        if(BlockEventHelper::grow($side, $grow, null)){
+                            $crop->getPosition()->getWorld()->setBlock($crop->getPosition(), $crop->setFacing($facing));
+                        }
+                    }
+                }
+            }
+            if ($crop instanceof MelonStem) {
+                if ($crop->getAge() >= MelonStem::MAX_AGE) {
+                    $grow = VanillaBlocks::MELON();
+                    foreach(Facing::HORIZONTAL as $side){
+                        if($crop->getSide($side)->hasSameTypeId($grow)){
+                            return;
+                        }
+                    }
+                    $facing = Facing::HORIZONTAL[array_rand(Facing::HORIZONTAL)];
+                    $side = $crop->getSide($facing);
+                    if($side->getTypeId() === BlockTypeIds::AIR && $side->getSide(Facing::DOWN)->hasTypeTag(BlockTypeTags::DIRT)){
+                        if(BlockEventHelper::grow($side, $grow, null)){
+                            $crop->getPosition()->getWorld()->setBlock($crop->getPosition(), $crop->setFacing($facing));
+                        }
+                    }
                 }
             }
         }
--- src/bulutbulutsun/listener/EventListener.php
+++ src/bulutbulutsun/listener/EventListener.php
@@ -5,6 +5,9 @@ namespace bulutbulutsun\listener;
 use bulutbulutsun\Loader;
 use pocketmine\block\Crops;
 use pocketmine\block\Farmland;
+use pocketmine\block\NetherWartPlant;
+use pocketmine\block\SoulSand;
+use pocketmine\block\VanillaBlocks;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\block\BlockSpreadEvent;
@@ -22,10 +25,10 @@ class EventListener implements Listener
         if ($event->isCancelled()) return;
         $block = $event->getBlock();
         $position = $block->getPosition();
-        if ($block instanceof Farmland) {
+        if ($block instanceof Farmland || $block instanceof SoulSand) {
             Loader::getInstance()->deleteData($position->getWorld()->getFolderName(), $position->getX(), $position->getY() + 1, $position->getZ());
         }
-        if ($block instanceof Crops) {
+        if ($block instanceof Crops || $block instanceof NetherWartPlant) {
             Loader::getInstance()->deleteData($position->getWorld()->getFolderName(), $position->getX(), $position->getY(), $position->getZ());
         }
     }
@@ -33,7 +36,7 @@ class EventListener implements Listener
         $block = $event->getBlock();
         $position = $block->getPosition();
         $crops = new Position($position->getX(), $position->getY() + 1, $position->getZ(), $position->getWorld());
-        if ($position->getWorld()->getBlock($crops) instanceof Crops) {
+        if ($position->getWorld()->getBlock($crops) instanceof Crops || $position->getWorld()->getBlock($crops) instanceof NetherWartPlant) {
             Loader::getInstance()->deleteData($position->getWorld()->getFolderName(), $position->getX(), $position->getY() + 1, $position->getZ());
         }
     }
@@ -41,18 +44,19 @@ class EventListener implements Listener
     {
         $blocks = $event->getBlock()->getAffectedBlocks();
         foreach ($blocks as $block) {
-            if ($block instanceof Crops) {
+            if ($block instanceof Crops || $block instanceof NetherWartPlant) {
                 $position = $block->getPosition();
                 Loader::getInstance()->deleteData($position->getWorld()->getFolderName(), $position->getX(), $position->getY(), $position->getZ());
             }
         }
     }
+
     public function onBlockPlace(BlockPlaceEvent $event){
         if ($event->isCancelled()) return;
         $item = $event->getItem();
         $block = $event->getBlockAgainst();
         $position = $block->getPosition();
-        $seeds = [VanillaItems::WHEAT_SEEDS(), VanillaItems::PUMPKIN_SEEDS(), VanillaItems::MELON_SEEDS(), VanillaItems::BEETROOT_SEEDS()];
+        $seeds = [VanillaItems::WHEAT_SEEDS(), VanillaItems::PUMPKIN_SEEDS(), VanillaItems::MELON_SEEDS(), VanillaItems::BEETROOT_SEEDS(), VanillaBlocks::NETHER_WART_BLOCK()->asItem()];
         foreach ($seeds as $seed) {
             if ($item instanceof $seed) {
                 Loader::getInstance()->setData($position->getWorld()->getFolderName(), $position->getX(), $position->getY() + 1, $position->getZ());
